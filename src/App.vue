ac62d35b7db681a75df92abc6a0aae4e03b3de55
ewrf3 <q304062@163.com>
2022-01-09 02:52:33 +0800

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <h1>xxxxxxxxxx</h1>
+        <i class="el-icon-edit"></i>
+        <i class="el-icon-share"></i>
+        <i class="el-icon-delete"></i>
+        <el-button type="primary" icon="el-icon-search">搜索</el-button>
+        <img :src="img" alt="">
     </div>
-    <router-view/>
-  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      img:''
+    }
+  },
+  mounted(){
+    this.$axios.get('https://api.thecatapi.com/v1/images/search?limit=1').then(res=>{
+      console.log('img',res)
+      this.img = res.data[0].url
+    }).catch(err => {
+      console.log('err',err)
+    })
+    this.$axios.get('http://localhost:3000/users/find').then(res=>{
+      console.log('users',res)
+    }).catch(err => {
+      console.log('err',err)
+    })
+  },
+  methods: {},
+}
+</script>
 
 <style lang="less">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
 }
 </style>
